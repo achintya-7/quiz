cde0fdb0a67813153abbf6a39bb1b26539ace531
Achintya <achintya22052000@gmail.com>
2023-03-16 19:05:39 +0530

    added home page and proto files

---
+++ quiz_app/lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:quiz_app/feature/home/views/home.dart';
+
+List<GetPage> routes = [
+  GetPage(
+    name: '/',
+    page: () => const Home(),
+  ),
+];
